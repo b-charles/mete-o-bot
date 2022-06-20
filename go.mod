609b8d2d561c93303a2b79d4c1c5c2b9abad6562
Benoit Charles <19323162+b-charles@users.noreply.github.com>
2022-06-20 19:02:45 +0200

    Add mail sender

--- go.mod
+++ go.mod
@@ -7,4 +7,6 @@ require github.com/PuerkitoBio/goquery v1.8.0
 require (
 	github.com/andybalholm/cascadia v1.3.1 // indirect
 	golang.org/x/net v0.0.0-20210916014120-12bc252f5db8 // indirect
+	gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
+	gopkg.in/gomail.v2 v2.0.0-20160411212932-81ebce5c23df // indirect
 )
--- main.go
+++ main.go
@@ -1,8 +1,14 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
+	"os"
+	"strconv"
 	"strings"
+
+	"gopkg.in/gomail.v2"
 )
 
 type Message struct {
@@ -10,19 +16,81 @@ type Message struct {
 	Body  string
 }
 
-func formatMessages(messages []Message) string {
+func bodyMessage() string {
+
+	var messages = []Message{
+		Meteopnm(),
+		Citocin(),
+		Bonjourmadamevibdy()}
+
 	var builder strings.Builder
 	for _, msg := range messages {
 		fmt.Fprintf(&builder, "%s <br>~~~~~~~~~<br><br> %s <br><br>", msg.Title, msg.Body)
 	}
 	return builder.String()
+
 }
 
-var messages = []Message{
-	Meteopnm(),
-	Citocin(),
-	Bonjourmadamevibdy()}
+type Recipient struct {
+	Name    string
+	Address string
+}
+
+func mailTo() []Recipient {
+
+	var r []Recipient
+	if err := json.Unmarshal([]byte(os.Getenv("MAIL_TO")), &r); err != nil {
+		panic(err)
+	}
+
+	return r
+
+}
 
 func main() {
-	fmt.Printf(formatMessages(messages))
+
+	subject := fmt.Sprintf("Mété-O-BOT %s", today())
+	body := bodyMessage()
+
+	host := os.Getenv("SMTP_HOST")
+	if host == "" {
+		fmt.Println(subject)
+		fmt.Println("---")
+		fmt.Println(body)
+		return
+	}
+
+	port, err := strconv.Atoi(os.Getenv("SMTP_PORT"))
+	if err != nil {
+		panic(err)
+	}
+
+	user := os.Getenv("SMTP_USER")
+	password := os.Getenv("SMTP_PASSWORD")
+
+	dialer := gomail.NewDialer(host, port, user, password)
+	sender, err := dialer.Dial()
+	if err != nil {
+		panic(err)
+	}
+
+	from := os.Getenv("MAIL_FROM")
+
+	message := gomail.NewMessage()
+
+	for _, recipient := range mailTo() {
+
+		message.SetHeader("From", from)
+		message.SetAddressHeader("To", recipient.Address, recipient.Name)
+		message.SetHeader("Subject", subject)
+		message.SetBody("text/html", body)
+
+		if err := gomail.Send(sender, message); err != nil {
+			log.Printf("Could not send email to %v (%v): %v", recipient.Name, recipient.Address, err)
+		}
+
+		message.Reset()
+
+	}
+
 }
